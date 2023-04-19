4456956a5173849b5c4c594ec249c2a7c31c673a
Laurel Rubel <laurel.rubel@gmail.com>
2023-04-18 20:43:09 -0600

    Updated HTML and CSS, added new JS content

--- script.js
+++ script.js
@@ -1,3 +1,41 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
-document.querySelector("body").appendChild(h2);
+const dodger = document.getElementById("dodger");
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+    if (left > 0) {
+        dodger.style.left = `${left - 1}px`;
+    }
+
+}
+
+function moveDodgerRight() {
+    const rightNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(rightNumbers, 10);
+    if (left < 360) {
+        dodger.style.left = `${left + 1}px`;
+    }
+}
+
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+        moveDodgerLeft();
+    } else if (e.key === "ArrowRight") {
+        moveDodgerRight();
+    }
+});
+
+const hover = document.getElementById("nav-bar");
+hover.addEventListener("mouseover", (event) => {
+    event.target.style.color = "#0a9396";
+});
+hover.addEventListener("mouseout", (e) => {
+    e.target.style.color = "#e9d8a6";
+});
+
+const hoverboard = document.getElementById("contact-link");
+hoverboard.addEventListener("mouseover", (event) => {
+    event.target.style.color = "#0a9396";
+});
+hoverboard.addEventListener("mouseout", (e) => {
+    e.target.style.color = "#e9d8a6";
+});
